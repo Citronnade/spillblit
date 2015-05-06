f82e70eca072fb8f5ab392fd410949b2ce85c020
Max Zhou <ball908765@gmail.com>
2015-05-06 12:33:16 -0400

    stopped the errors

--- client/client.js
+++ client/client.js
@@ -21,6 +21,34 @@ Template.joinForm.events({
 
 	var data = {'fname': fname, 'lname': lname, 'table': table};
 	console.log(data);
+	Meteor.call("addTable", data);
     }
     
 });
+
+var getBills = function(tData){ //a function that calls a function. oh boy.
+    var totalBills = _.reduce(tData, function(memo, person){
+	memo["100"]+=person.cash["100"];
+	memo["50"]+=person.cash["50"];
+	memo["20"]+=person.cash["20"];
+	memo["10"]+=person.cash["10"];
+	memo["5"]+=person.cash["5"];
+	memo["2"]+=person.cash["2"];
+	memo["1"]+=person.cash["1"];
+
+	return memo;
+	
+    }, {
+	"20": 0,
+	"10": 0,
+	"5": 0,
+	"2": 0,
+	"1": 0
+    });
+    
+
+    return totalBills;
+
+};
+
+//console.log(getBills(tableData));
