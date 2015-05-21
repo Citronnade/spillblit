04bf5eef25a9e53c8f3c302717b879e0a822e7b3
Sean Yip <bouowmx@gmail.com>
2015-05-21 13:16:35 -0400

    Milestone: Other people can add their bills to a table

--- client/client.js
+++ client/client.js
@@ -6,13 +6,21 @@ Template.create.events({
 				Session.set("_id", result);
 				Session.set("name", $("#name").val());
 				console.log("_id: " + Session.get("_id"));
-				Router.go("/table/" + Session.get("_id") + "/enterBills");
+				Router.go("/table/" + Session.get("_id"));
 			}
 		});
 	}
 });
 
-Template.enterBills.events({
+Template.join.events({"click #join": function() {
+	Session.set("_id", $("#id").val());
+	Router.go("/table/" + Session.get("_id"));
+}});
+
+Template.registerHelper("hasSession", function(name) {return (Session.get(name)) ? true : false;});
+Template.registerHelper("session", function(name) {return Session.get(name);});
+
+Template.table.events({
 	"click paper-icon-button": function(element) {
 		if (element.target.id.search("add") >= 0) {
 			document.getElementById(element.target.id.slice(3)).value++;
@@ -27,6 +35,7 @@ Template.enterBills.events({
 		}
 	}, 
 	"click #submit": function() {
+		Session.set("name", $("#name").val());
 		var bills = {_id: Session.get("_id"), name: Session.get("name"), bills: {}}, denominations = ["0.01", "0.05", "0.10", "0.25", "0.50", "1", "2", "5", "10", "20", "50", "100"];
 		for (var i = 0; i < denominations.length; i++) {bills.bills[denominations[i].replace(".", ",")] = document.getElementById(denominations[i]).value;}
 		Meteor.call("enterBills", bills);
@@ -38,6 +47,10 @@ Template.enterBills.events({
 	}
 });
 
+Template.table.helpers({
+	getTable: function(_id) {return objectToArray(Tables.findOne(_id));}
+});
+
 var enterBillsTotal = function() {
 	var total = 0, denominations = ["0.01", "0.05", "0.10", "0.25", "0.50", "1", "2", "5", "10", "20", "50", "100"];
 	for (var i = 0; i < denominations.length; i++) {total += Number(document.getElementById(denominations[i]).value) * Number(denominations[i]);}
@@ -45,21 +58,6 @@ var enterBillsTotal = function() {
 	while (totalElement.firstChild) {totalElement.removeChild(totalElement.firstChild);}
 	totalElement.appendChild(document.createTextNode(total.toFixed(2)));
 };
-
-Template.join.events({
-	"click #join": function() {Router.go("/table/" + $("#id").val());}
-});
-
-Template.registerHelper("hasSession", function(name) {return (Session.get(name)) ? true : false;});
-Template.registerHelper("session", function(name) {return Session.get(name);});
-
-Template.table.events({
-	"click #enterBills": function() {Router.go("./enterBills");}
-});
-
-Template.table.helpers({
-	getTable: function(_id) {return objectToArray(Tables.findOne(_id));}
-});
 
 function objectToArray(object) {return _.map(object, function(value, key, list) {return " " + key.replace(",", ".") + ": " + ((typeof value == "object") ? objectToArray(value) : value);});}
 
